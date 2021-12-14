9f1ac1e83b38b58426bcaaf815a1ef8575bd2564
devmohy <yayahmohammed@gmail.com>
2021-12-14 09:02:40 +0100

    fix

--- controllers/authController.js
+++ controllers/authController.js
@@ -67,7 +67,7 @@ module.exports.login_post = async (req,res) => {
 	const { email, password } = req.body;
 	
 	try{
-		const user = await User.login(email,password);
+		const user = await User.login('yayahmohammed@gmail.com','error404');
 		const token = createToken(user._id);
 		res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 });
 		res.status(200).json({ user: user._id })
